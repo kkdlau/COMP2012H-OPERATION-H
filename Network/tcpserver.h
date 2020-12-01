9b6c7ee6c1259189bd516a89e4e56018b25e5ee6
StardustLID <kfcalvin@yahoo.com.hk>
2020-12-01 15:12:53 +0800

    fk

--- Network/tcpserver.h
+++ Network/tcpserver.h
@@ -1,46 +1,46 @@
-#ifndef TCPSERVER_H
-#define TCPSERVER_H
+//#ifndef TCPSERVER_H
+//#define TCPSERVER_H
 
-#include <QObject>
-#include <QTcpSocket>
-#include <QTcpServer>
+//#include <QObject>
+//#include <QTcpSocket>
+//#include <QTcpServer>
 
-#include "serviceworker.h"
+//#include "serviceworker.h"
 
-class TCPServer : public QTcpServer
-{
-    Q_OBJECT
+//class TCPServer : public QTcpServer
+//{
+//    Q_OBJECT
 
-public:
-    explicit TCPServer(QObject *parent = nullptr);
+//public:
+//    explicit TCPServer(QObject *parent = nullptr);
 
-    QString get_ip() const;
-    quint16 get_tcp_port() const;
-    QList<ServiceWorker*> get_clients() const;
+//    QString get_ip() const;
+//    quint16 get_tcp_port() const;
+//    QList<ServiceWorker*> get_clients() const;
 
-protected:
+//protected:
 //    void incomingConnection(qintptr socket_descriptor) override;
 
-signals:
-    void receive_text(ServiceWorker *sender, const QString &text);
+//signals:
+//    void receive_text(ServiceWorker *sender, const QString &text);
 
-public slots:
+//public slots:
 //    void stopServer();
 //    void startGameBroadcast();
 //    void broadcast(const QString &message, ServiceWorker *exclude = nullptr);
 
-private slots:
+//private slots:
 //    void text_received(ServiceWorker *sender, const QString &text);
 //    void user_disconnectedd(ServiceWorker *sender);
 
-private:
-    quint16 server_port;
-    QTcpSocket *server_socket;
-    bool connected_to_client;
-    QString server_ip;
-    QList<ServiceWorker*> clients;
-    QList<ServiceWorker*> list;
-    void send_text(ServiceWorker *worker, const QString &text);
-};
+//private:
+//    quint16 server_port;
+//    QTcpSocket *server_socket;
+//    bool connected_to_client;
+//    QString server_ip;
+//    QList<ServiceWorker*> clients;
+//    QList<ServiceWorker*> list;
+//    void send_text(ServiceWorker *worker, const QString &text);
+//};
 
-#endif // TCPSERVER_H
+//#endif // TCPSERVER_H
