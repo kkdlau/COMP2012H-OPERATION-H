7f6fd521e1597c3047bbb45be422db9278280e1b
almost1234 <58636438+almost1234@users.noreply.github.com>
2020-12-04 15:31:23 +0800

    Documentation again
    overlay, weapon, bullet, enemy and stuff

--- NewWeapon/weaponmanager.h
+++ NewWeapon/weaponmanager.h
@@ -8,17 +8,50 @@ class WeaponManager : public QObject
 {
     Q_OBJECT
 public:
-
     ~WeaponManager();
-    void AddWeapon(Weapon*);
-    void DeleteWeapon(Weapon*);
+    /**
+     * @brief AddWeapon add Weapon* to the weaponDatabase
+     */
+    void AddWeapon(Weapon* weapon);
+    /**
+     * @brief DeleteWeapon remove Weapon* from the weaponDatabase
+     * @param weapon is the Weapon* data what wants to be added
+     */
+    void DeleteWeapon(Weapon* weapon);
+    /**
+     * @brief GenerateRandomWeapon creates random weapon from the QRandomGenerator
+     * @param weapon is the target Weapon* to be removed
+     * @return Weapon*
+     */
     Weapon* GenerateRandomWeapon();
+    /**
+     * @brief GetWeaponDatabase return the weaponDatabase
+     * @return QList<Weapon*>
+     */
     const QList<Weapon*> GetWeaponDatabase() const;
+    /**
+     * @brief GetWeapon get the specified
+     * @param weaponId
+     * @return the Weapon* with the specified weaponId
+     */
     Weapon* GetWeapon(int weaponId);
+    /**
+     * @brief getInstance
+     * @return return the WeaponManager Instance. If Instance is nullptr, a WeaponManager will be created instead
+     */
     static WeaponManager* getInstance();
 private:
-        WeaponManager();
+    /**
+     * @brief WeaponManager constructor
+     */
+    WeaponManager();
+    /**
+     * @brief Instance of the WeaponManager
+     */
     static WeaponManager* Instance;
+    /**
+     * @brief weaponDatabase is a list of Weapon*
+     */
     QList<Weapon*> weaponDatabase;
 
 };
--- Weapons/bullet.h
+++ Weapons/bullet.h
@@ -10,14 +10,40 @@ class Bullet : public QObject,public QGraphicsPixmapItem
 {
 
 public:
+    /**
+     * @brief Bullet constructor creates the bullet when RangedWeapon attack
+     * @param damage sets the bullet damage when hit Character*
+     * @param angle sets the bullet angle tragectory
+     * @param x set the X position in the map
+     * @param y set the Y position in the map
+     * @param owner sets the pointer of the RangedWeapon owner to prevent self collsiion
+     */
     Bullet(int damage, int angle, int x, int y, QGraphicsItem* owner);
     ~Bullet();
+    /**
+     * @brief move the bullet everytime the timer is called
+     */
     void move();
 private:
+    /**
+     * @brief owner sets the RangedWeapon owner
+     */
     QGraphicsItem * owner;
+    /**
+     * @brief bullet damage when collide with Character*
+     */
     const int damage;
+    /**
+     * @brief angle tragectory of where the bullet move
+     */
     const int angle;
+    /**
+     * @brief bulletSpeed set how fast the bullet being shot
+     */
     const int bulletSpeed = 3;
+    /**
+     * @brief lifeSpan set the bullet lifespan before destroying itself
+     */
     int lifeSpan = 100;
     QTimer timer;
 };
